31510b3b1224a5389bb38b4080bcf0105d7ba3ab
steven <sjraaijmakers@gmail.com>
2025-08-24 16:22:13 +0200

    another one

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,22 @@ import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [
+    astroI18next({
+      defaultLocale: "en",
+      locales: ["en", "th"],
+      i18next: {
+        debug: false,
+        initImmediate: false,
+        backend: {
+          loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+        },
+      },
+      i18nextPlugins: {
+        fsBackend: "i18next-fs-backend",
+      },
+    })
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "th"]
