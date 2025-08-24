44328096edfe8731488d1e5e3f92af90534892e2
steven <sjraaijmakers@gmail.com>
2025-08-24 15:31:35 +0200

    cleaned

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -2,4 +2,14 @@
 export default {
   defaultLocale: "en",
   locales: ["en", "th"],
+  i18next: {
+    debug: false,
+    initImmediate: false,
+    backend: {
+      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+    },
+  },
+  i18nextPlugins: {
+    fsBackend: "i18next-fs-backend",
+  },
 };
