71aa274c26080602dfa36c23b2cdb13a783c40bb
steven <sjraaijmakers@gmail.com>
2025-08-24 17:06:03 +0200

    mmmm

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "en",
+    locales: ["en", "th"],
+    i18next: {
+      debug: false,
+      initImmediate: false,
+      backend: {
+        loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+      },
+    },
+    i18nextPlugins: {
+      fsBackend: "i18next-fs-backend",
+    },
+  };
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,7 @@ import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    astroI18next({
-      defaultLocale: "en",
-      locales: ["en", "th"],
-      i18next: {
-        debug: false,
-        initImmediate: false,
-        backend: {
-          loadPath: "./public/locales/{{lng}}/{{ns}}.json",
-        },
-      },
-      i18nextPlugins: {
-        fsBackend: "i18next-fs-backend",
-      },
-    })
-  ],
+  integrations: [astroI18next()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "th"]
